a35baa6b30d390aa5830a2e47add64a6724d5818
dataisland <dataisland@outlook.com>
2025-03-14 00:29:31 +0000

    Update undefined sanitizer parser

--- patchagent/parser/undefined.py
+++ patchagent/parser/undefined.py
@@ -2,14 +2,12 @@ import re
 from pathlib import Path
 from typing import List, Optional, Tuple
 
-from patchagent.logger import log
 from patchagent.parser.address import AddressSanitizerReport
 from patchagent.parser.cwe import CWE, CWE_DESCRIPTIONS, CWE_REPAIR_ADVICE
 from patchagent.parser.sanitizer import Sanitizer, SanitizerReport
-from patchagent.parser.utils import guess_relpath
+from patchagent.parser.utils import simplify_and_extract_stacktraces
 
-UndefinedBehaviorPattern = r"(runtime error: .+?SUMMARY: [^\n]+)"
-StackTracePattern = r"^\s*#(\d+)\s+(0x[\w\d]+)\s+in\s+(.+)\s+(/.*)\s*"
+UndefinedBehaviorPattern = r"(runtime error: .+)"
 ANSIEscape = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
 
 
@@ -51,59 +49,16 @@ class UndefinedBehaviorSanitizerReport(SanitizerReport):
             return None
 
         content = match.group(1)
-        lines = content.splitlines()
-
         body = []
-        stacktraces: List[List[Tuple[str, Path, int, int]]] = [[]]
-        current_count = -1
-
-        for line in lines:
-            if line.strip().startswith("SUMMARY:"):
+        for line in content.splitlines():
+            if line.startswith("SCARINESS") or line.startswith("DEDUP_TOKEN"):
+                continue
+            if line.startswith("SUMMARY:"):
                 break
+            body.append(line)
 
-            if line.strip().startswith("#"):
-                count = int(line.split()[0][1:])
-                if count == current_count + 1:
-                    current_count += 1
-                else:
-                    stacktraces.append([])
-                    current_count = 0
-                    assert count == 0
-
-                if (match := re.search(StackTracePattern, line)) is not None:
-                    function_name = match.group(3)
-                    entries = match.group(4).split(":")
-
-                    while len(entries) > 3 or (len(entries) > 1 and any(not c.isdigit() for c in entries[1:])):
-                        entries[0] = entries[0] + ":" + entries[1]
-                        entries.pop(1)
-
-                    if len(entries) == 0:
-                        continue
-
-                    while len(entries) < 3:
-                        entries.append("0")
-                    filepath, line_number, column_number = entries
-                    assert filepath.startswith("/")
-
-                    normpath = Path(filepath).resolve()
-                    desc = f"{normpath}:{line_number}:{column_number}"
-                    if f"{filepath}:{line_number}:{column_number}" != match.group(4):
-                        log.warning(f"Incomplete file path: {desc} vs {match.group(4)}")
-
-                    if work_path is not None and normpath.is_relative_to(work_path):
-                        stacktraces[-1].append((function_name, normpath.relative_to(work_path), int(line_number), int(column_number)))
-                    elif (relpath := guess_relpath(source_path, normpath)) is not None:
-                        stacktraces[-1].append((function_name, relpath, int(line_number), int(column_number)))
-
-                    if work_path is None:
-                        body.append(f"    - {function_name} {desc}")
-                    elif normpath.is_relative_to(work_path):
-                        body.append(f"    - {function_name} {normpath.relative_to(work_path)}:{line_number}:{column_number}")
-            else:
-                body.append(re.sub(r"==[0-9]+==", "", line))
-
-        return UndefinedBehaviorSanitizerReport(content, CWE.Undefined_behavior, stacktraces[0], "\n".join(body), stacktraces[1:])
+        simplified, stacktraces = simplify_and_extract_stacktraces(body, source_path, work_path)
+        return UndefinedBehaviorSanitizerReport(content, CWE.Undefined_behavior, stacktraces[0], simplified, stacktraces[1:])
 
     @property
     def summary(self) -> str:
